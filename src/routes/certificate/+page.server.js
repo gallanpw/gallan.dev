32f35a6d0f9348ceddbdcd61de59e10a590ed166
gallanpw <gallan.widyanto@gmail.com>
2025-08-13 15:57:13 +0700

    Feat: Change cloudflare cdn to cloudinary cdn for image certificate

--- src/routes/certificate/+page.server.js
+++ src/routes/certificate/+page.server.js
@@ -2,37 +2,37 @@ export async function load() {
     const certificates = [
         {
             title: "Dynamic Web (PHP & MySQL)",
-            thumbnail: "https://imagedelivery.net/ocG287Hqx13muJG7zfKTyA/3f7adadf-d4da-43e4-36d3-a35dbde59800/public",
+            thumbnail: "https://res.cloudinary.com/gallanpw/image/upload/v1755072234/ral1i2byrycpwykhljvm.webp",
             link: "https://drive.google.com/file/d/1WAuM4ks57HuljckNGjrcmRFLgpLRTT8z/view",
             date: "2014-09-17"
         },
         {
             title: "Intro to Software Engineering",
-            thumbnail: "https://imagedelivery.net/ocG287Hqx13muJG7zfKTyA/5290cb0c-87b8-4e4f-c971-e89e28c80700/public",
+            thumbnail: "https://res.cloudinary.com/gallanpw/image/upload/v1755072234/i1adwn73nwbwms169hx7.webp",
             link: "https://drive.google.com/file/d/1Ajk-zU6kIiFeFNE6vX0f75RT1y5_CN59/view",
             date: "2023-10-27"
         },
         {
             title: "Fullstack Web Development",
-            thumbnail: "https://imagedelivery.net/ocG287Hqx13muJG7zfKTyA/b03c86ee-3de0-43cd-8618-61fd8db50f00/public",
+            thumbnail: "https://res.cloudinary.com/gallanpw/image/upload/v1755072233/p0sguxfzidzfobyhfdhi.webp",
             link: "https://drive.google.com/file/d/1FeNfVe2KdaDJmYc9urcOQnv4U9reLbIB/view",
             date: "2023-11-02"
         },
         {
             title: "Laravel Web Development",
-            thumbnail: "https://imagedelivery.net/ocG287Hqx13muJG7zfKTyA/f5eac7f4-1899-43e6-5fc2-2935dec01d00/public",
+            thumbnail: "https://res.cloudinary.com/gallanpw/image/upload/v1755072234/u06rrcewhs6rmro5uwha.webp",
             link: "https://drive.google.com/file/d/1yx8uzrhSliTczjJ3OKpt0iqPTEs_vOxh/view",
             date: "2025-03-13"
         },
         {
             title: "NodeJS Backend Development with Express JS",
-            thumbnail: "https://imagedelivery.net/ocG287Hqx13muJG7zfKTyA/a6de02df-7b65-4895-5d9f-f3e18b195600/public",
+            thumbnail: "https://res.cloudinary.com/gallanpw/image/upload/v1755072234/vjgqrgtcuzwhbtqtgjpb.webp",
             link: "https://drive.google.com/file/d/1GsHrpgD6R4vHK9WIZS0q18sqxIIcN46c/view",
             date: "2025-05-24"
         },
         {
             title: "React JS Web Frontend Development",
-            thumbnail: "https://imagedelivery.net/ocG287Hqx13muJG7zfKTyA/2a1d28f9-095e-47d9-daee-30e2b0854c00/public",
+            thumbnail: "https://res.cloudinary.com/gallanpw/image/upload/v1755072234/kdmej9c55z0domrrbqye.webp",
             link: "https://drive.google.com/file/d/1HdQAWC0mIoGSsXm1b49kGB7unXv7IwqW/view",
             date: "2025-06-26"
         }
